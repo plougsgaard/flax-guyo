4e29cfac0aa09428b9b5685b65cfd69d53d12dfb
plougsgaard <thomas@plougsgaard.dk>
2018-03-23 23:28:54 +0100

    Cards will save humanity.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import AceEditor from 'react-ace'
+import Button from 'material-ui/Button'
+import Card, { CardContent } from 'material-ui/Card'
+import Grid from 'material-ui/Grid'
 
 import 'brace/mode/java'
 import 'brace/theme/github'
@@ -28,18 +31,30 @@ class App extends React.Component {
 
   render () {
     return (
-      <div>
-        <AceEditor
-          style={{ width: '500px', height: '100px' }}
-          mode='java'
-          theme='github'
-          onChange={input => this.setState({ input }, this.handleSendMessage)}
-          value={this.state.input}
-          editorProps={{ $blockScrolling: true }} />
-        <h1>Output</h1>
-        <p>Status: {this.state.output.status}</p>
-        <p>Result: {this.state.output.result}</p>
-      </div>
+      <Grid container={true} spacing={24}>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <AceEditor
+              style={{ width: '100%', height: '100px' }}
+              mode='java'
+              theme='github'
+              onChange={input => this.setState({ input }, this.handleSendMessage)}
+              value={this.state.input}
+              editorProps={{ $blockScrolling: true }} />
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardContent>
+              <Button variant='fab' color='secondary'>Magnus</Button>
+              <Button variant='raised' color='primary'>Magnus</Button>
+              <h1>Output</h1>
+              <p>Status: {this.state.output.status}</p>
+              <p>Result: {this.state.output.result}</p>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     )
   }
 }
